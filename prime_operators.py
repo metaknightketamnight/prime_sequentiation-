448a7f23875a4c6163761afc064477c20dbc6fab
metaknightketamnight <39011943+metaknightketamnight@users.noreply.github.com>
2024-02-16 00:34:10 -0800

    add details later, too tired

--- prime_operators.py
+++ prime_operators.py
@@ -1,16 +1,27 @@
 import prime_finders
 
 def oddPrimesVsNonPrimes(maxCheck) :
-	print('# of primes:',len(brutePrimesHard(maxCheck)))
-	print('# of non-prime odds:',len(bruteOddNonPrimes(maxCheck)))
+	"""
+	- bPH-based print function
+	- checks length of prime to non odd prime
+	"""
+	print('# of primes:',len(prime_finders.brutePrimesHard(maxCheck)))
+	print('# of non-prime odds:',len(prime_finders.bruteOddNonPrimes(maxCheck)))
 
 
-def findDifferenceList(primeList) :
+def findDifferenceList(l) :
+	"""
+	- general (hence 'l') method of discovering a list of differences between entries of original list
+	"""
 	differenceList = []
-	for i in range(1,len(primeList)) :
-		differenceList.append(primeList[i] - primeList[i-1])
+	for i in range(1,len(l)) :
+		differenceList.append(l[i] - l[i-1])
+		print(f'Difference between {l[i]} and {l[i-1]}: {l[i]-l[i-1]}')
 
+	assert len(differenceList) == len(l) - 1
 	return differenceList
 
-oddPrimesVsNonPrimes(5)
-# print(findDifferenceList(brutePrimesHard(input())))
+
+user_in = int(input())
+print(prime_finders.brutePrimesHard(user_in))
+findDifferenceList(prime_finders.brutePrimesHard(user_in))
